2b7d19cc7863e3da2879a3a2b6599238ae100e36
lcc2207 <lynn@fidmail.com>
2018-10-02 12:59:09 -0500

    update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,3 +8,8 @@ pip install docker-compose
 
 curl -fsSL https://get.docker.com/ | sh
 service docker start || systemctl start docker
+
+git clone https://github.com/scalr-tutorials/scalr-reporting-webhook /opt/reporting-webhook
+
+chmod 755 /opt/reporting-webhook/relaunch.sh
+/opt/reporting-webhook/relaunch.sh
